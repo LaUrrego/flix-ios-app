29a73eb54a466775ed3fa44f89eff56d40258ba1
LaUrrego <lurrego22@gmail.com>
2022-09-20 21:57:26 +0200

    Add detail view to cells

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -26,8 +26,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
         // Do any additional setup after loading the view.
         
-        print("hello")
-        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -39,7 +37,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                  
                     self.movies = dataDictionary["results"] as! [[String: Any]]
-                    print(dataDictionary)
                  self.tableView.reloadData()
 
                     // TODO: Get the array of movies
@@ -79,14 +76,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("Loading up the details screen here")
+     
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        // Pass the selected movie to the view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        // Deselects the previously selected cell, personal UI clean-up preference to make going back look cleaner. 
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
