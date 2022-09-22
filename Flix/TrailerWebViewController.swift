c7c1766a74dd126bbf33e9fa45893bea47171e53
LaUrrego <lurrego22@gmail.com>
2022-09-22 17:19:21 +0200

    Upload repo copy of part 2 GIF

---
+++ Flix/TrailerWebViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TrailerWebViewController.swift
+//  Flix
+//
+//  Created by Larry Urrego on 9/22/22.
+//
+
+import UIKit
+import WebKit
+
+class TrailerWebViewController: UIViewController {
+    
+    private let webView: WKWebView
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
